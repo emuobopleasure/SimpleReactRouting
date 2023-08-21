de443c6f4d70c615fef85a6617a22948e2f7ad4a
Davis06 <davis06@outlook.com>
2023-08-21 22:20:43 +0100

    Implemented page navigation using the Link component

---
+++ src/components/MainNavigation.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MainNavigation = () => {
+  return (
+    <div>MainNavigation</div>
+  )
+}
+
+export default MainNavigation
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
+    <div>
     <h1>My HomePage</h1>
+    <p>go to <Link to='/products'>all products </Link></p>
+    </div>
   )
 }
 

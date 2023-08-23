c4c4bb80346282fa4cff7aa781dee656b64f3131
Davis06 <davis06@outlook.com>
2023-08-23 14:13:52 +0100

    Utilized the useNavigate hook to enable programmatic navigation within the application

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const HomePage = () => {
+
+    const navigate = useNavigate()
+
+    const navigateHandler = () => {
+        navigate('/products')
+    }
   return (
     <div>
     <h1>My HomePage</h1>
     <p>go to <Link to='/products'>all products </Link></p>
+    <button onClick={navigateHandler}>navigate</button>
     </div>
   )
 }

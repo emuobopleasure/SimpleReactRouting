f2d8f6cb19037dd179a9ced8e7b56fb1a7aec012
Davis06 <davis06@outlook.com>
2023-08-23 14:02:40 +0100

    Highlighted currently active links by utilizing the NavLink component

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import classes from '../components/MainNavigation.module.css'
 
 const MainNavigation = () => {
-  return (
-    <header className={classes.header}>
-        <nav>
-            <ul className={classes.list}>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/products'>Products</Link></li>
-            </ul>
-        </nav>
-    </header>
-  )
+    return (
+        <header className={classes.header}>
+            <nav>
+                <ul className={classes.list}>
+                    <li><NavLink to='/' className={({ isActive }) => isActive ? classes.active : undefined} end >Home</NavLink></li>
+                    <li><NavLink to='/products' className={({ isActive }) => isActive ? classes.active : undefined}>Products</NavLink></li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
 
 export default MainNavigation

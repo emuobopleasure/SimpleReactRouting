db1a46183e80f7c49876b1114d75c09c67ffd178
Davis06 <davis06@outlook.com>
2023-08-23 23:19:11 +0100

    Included links to dynamic routes, ensuring users can access specific content based on the dynamic parameters in the URL.

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,19 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ProductsPage = () => {
+    const PRODUCTS = [
+        { id: 'p1', title: 'product1' },
+        { id: 'p2', title: 'product2' },
+        { id: 'p3', title: 'product3' }
+    ]
   return (
     <>
     <h1>The Products Page</h1>
     <ul>
-        <li>
-            Product 1
-        </li>
-        <li>
-            Product 2
-        </li>
-        <li>
-            Product 3
-        </li>
+        { PRODUCTS.map((item) => (
+            <li key={item.id}>
+                <Link to={`/products/${item.id}`}>{item.title}</Link>
+            </li>
+        ))}
     </ul>
     </>
   )
